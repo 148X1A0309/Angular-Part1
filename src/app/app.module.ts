a3b574a6b4ad141f89d6a67daa242ab0d02720ce
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-24 18:59:23 +0530

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ServerComponent } from './Server/server/server.component';
+import { ServersComponent } from './Servers/servers/servers.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent ,ServerComponent, ServersComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
