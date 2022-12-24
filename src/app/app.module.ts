6d742d15d8ac89adb954f8e6c82dced1fff0fded
148X1A0309 <90388113+148X1A0309@users.noreply.github.com>
2022-12-24 20:51:07 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ServerComponent } from './Server/server/server.component';
 import { ServersComponent } from './Servers/servers/servers.component';
+import { DirectivesComponent } from './Assignment/directives/directives.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ,ServerComponent, ServersComponent],
+  declarations: [ AppComponent, HelloComponent ,ServerComponent, ServersComponent,DirectivesComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
